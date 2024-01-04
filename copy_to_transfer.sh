da8ebdeafbb99657d019269315b031498fadeb0b
Beatriz Lujan <blujantoro@oicr.on.ca>
2024-01-04 16:24:02 -0500

    update script based on IT recommendation

--- copy_to_transfer.sh
+++ copy_to_transfer.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
- 
 file=$1
 
-
 # Some nice variables
- 
 api_key=`cat ~/transfer.api.key`
 server="https://transfer.oicr.on.ca"
- 
- # Uploading the actual file and get attachment id for each file
-attachment_id=`curl -X POST --insecure --user "$api_key:x" -F Filedata=@$file $server/attachments`
+
+# Uploading the actual file and get attachment id for each file
+attachment_id=`curl -X POST -H "Accept: application/json" --insecure --user "$api_key:x" -T "$file" https://transfer.oicr.on.ca/attachments/binary_upload?filename=${file} | jq .attachment.id`
 echo $attachment_id
- 
+
 # Send the message
 #cat <<EOF | curl -s -X POST --insecure --user "$api_key:x" -H "Content-Type: application/json" -d @- $server/link
 #{"link":
