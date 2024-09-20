8312982804bf5484cbfd6104462ea62c074b230e
Richard Jovelin <rjovelin@oicr.on.ca>
2024-09-20 11:02:37 -0400

    modified command due to api endpoint change

--- copy_to_transfer.sh
+++ copy_to_transfer.sh
@@ -6,8 +6,18 @@ api_key=`cat ~/transfer.api.key`
 server="https://transfer.oicr.on.ca"
 
 # Uploading the actual file and get attachment id for each file
-attachment_id=`curl -X POST -H "Accept: application/json" --insecure --user "$api_key:x" -T "$file" https://transfer.oicr.on.ca/attachments/binary_upload?filename=${file} | jq .attachment.id`
+#attachment_id=`curl -X POST -H "Accept: application/json" --insecure --user "$api_key:x" -T "$file" https://transfer.oicr.on.ca/attachments/binary_upload?filename=${file} | jq .attachment.id`
+#echo $attachment_id
+
+
+attachment_id=`curl -X POST -H "Accept: application/json" --insecure --user "$api_key:x" -T "$file" https://transfer.oicr.on.ca/message/attachments/binary_upload?filename=${file} | jq .attachment.id`
 echo $attachment_id
+
+
+
+
+
+
 
 # Send the message
 #cat <<EOF | curl -s -X POST --insecure --user "$api_key:x" -H "Content-Type: application/json" -d @- $server/link
